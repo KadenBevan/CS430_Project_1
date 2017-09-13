69570795521a18854701852926d9d77e38e17b4e
Kaden Bevan <KadenBevan@gmail.com>
2017-09-13 10:31:00 -0700

    FUNCTIONAL: Added working main method. Added P6 --> P3 method. P6 --> P3 not functional, only does some error checking. Added structs for holding output.

--- ppmrw.c
+++ ppmrw.c
@@ -1,61 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-//represents a single pixel object
 typedef struct Pixel {
   unsigned char r, g, b;
 }Pixel;
 
-//this struct is used to store the entire image data, along with the width and height
 typedef struct Image {
   int width, height;
 }Image;
 
-int checkFile(char* file_in){
-	FILE * file;
-	file = fopen(file_in, "r");
-	if (file){
-	   fclose(file);
-	   return 1;
+
+int p3_p6(FILE* input_file, FILE* output_file){
+
+}
+int p6_p3(FILE* input_file, FILE* output_file){
+	char *buffer, *fh;
+	fh = (char*)malloc(sizeof(char) * 256);
+	fh = fgets(buffer, 256, input_file);
+	// If the file is empty
+	if(fh == NULL){
+		// ERROR: File is empty.
+		printf("ERROR");
+	}
+	// If the ppm file does not start with P6
+	// If the magic number is not correct.
+	if(buffer[0] != 'P' || buffer[1] != '6'){
+		// Still output the correct magic number.
+		// Let the user know that their file might be bad.
+		fprintf(output_file, "P3\n");
+		printf("ERROR");
 	}
 	else{
-	   return 0;
+		// remove the magic number from buffer and output it to the new file.
+		*(buffer+=2);
+		fprintf(output_file, "P3\n");
+		//printf("%c", buffer[0]);
+		if(){
+			*(buffer += 1);
+			printf("didnt crash");
+			//buffer+=1;
+		}
+		//while(n == ' ' || n == '\n'){
+		//	//buffer += 1;
+		//	printf("wtf");
+		//}
+		//while(getc(buffer) == '\n'){
+		//	buffer++;
+		//}
+		printf("%c", buffer[0]);
 	}
 }
-
-int p3_to_p6(char* in){
-    FILE *src, *dest;
-    char *outputFilename;
-    char magicNumber[3];
-    int height, width, depth;
-    unsigned char red, green, blue;
-	char c;
-    int i, j, widthCounter = 1;
-
-    if (!checkFile(in))
-    {
-        printf("FILE ERROR!");
-        return -1;
-    }
-    else
-        src = fopen(in, "r");
-	//while((c = fgetc(src)) != EOF) {
-    //    if (c == 'b') {
-    //        putchar(c);
-    //    }
-    //}
-	fscanf(src, "%s", &magicNumber);
-    if (strcmp(magicNumber, "P6") != 0){
-		printf("NOT IN MAGIC NUM");
-		printf("%s", magicNumber);
-		fclose(src);
-        return -1;
+int main(int argc, char *argv[]){
+	FILE *output_file = fopen(argv[3], "wb");
+	FILE *input_file = fopen(argv[2], "rb");
+  
+	char fh[256];
+	// Check for correct number of arguments
+	if(argc != 4){
+		// ERROR: This program takes 3 and only 3 arguments.
+		// Close Files
+		printf("ERROR. 1");
+		fclose(output_file);
+		fclose(input_file);
+		return -1;
 	}
-	printf("%s", magicNumber);
-	fclose(src);
-}
-
-int main(int argc, char* argv[]){
-	//FILE *fh_out = fopen(argv[2]);
-	p3_to_p6(argv[2]);
+	else{
+		// Number of arguments is correct. Continue as normal.
+		printf("Parsing Input.");
+	}
+	// Check for correct file types
+	if(strstr(argv[2], ".ppm") != NULL){
+		// Input file is the correct file type. Continue as normal.
+		printf("..\n");
+	}
+	else{
+		// ERROR: Not the correct filetype.
+		// Close Files
+		fclose(output_file);
+		fclose(input_file);
+		return -1;
+	}
+	
+	// Call correct write function
+	if( (strcmp(argv[1], "6") == 0) || (strcmp(argv[1], "3") == 0) ){
+		if(strcmp(argv[1], "6") == 0){
+			// write P3 --> P6  
+			p3_p6(input_file, output_file);
+		}
+		if(strcmp(argv[1], "3") == 0){
+			// write P6 --> P3
+			p6_p3(input_file, output_file);
+		}
+	}
+	else{
+		// ERROR: This program is only set up to convert P6 --> P3 or vice-versa.
+		// Close Files
+		fclose(input_file);
+		fclose(output_file);
+		return -1;
+	}
+	fclose(input_file);
+	fclose(output_file);
 }
