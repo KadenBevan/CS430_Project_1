88e167a0054a109118d5284cebd6a44a1dd83f8a
Kaden Bevan <KadenBevan@gmail.com>
2017-09-14 13:40:03 -0700

    FUNCTIONAL: ppmrw.c parses most allowed headers. ppmrw.c can write p3-->p6 and p6-->p3 with few errors.

--- ppmrw.c
+++ ppmrw.c
@@ -1,62 +1,230 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 typedef struct Pixel {
   unsigned char r, g, b;
 }Pixel;
 
-typedef struct Image {
-  int width, height;
-}Image;
+typedef struct Header{
+	char *magic_num;
+	int height;
+	int width;
+	int max_num;
+	Pixel *data;
+}header;
 
+int p3_p6(FILE*, FILE*);
+int p6_p3(FILE*, FILE*);
 
 int p3_p6(FILE* input_file, FILE* output_file){
-
-}
-int p6_p3(FILE* input_file, FILE* output_file){
-	char *buffer, *fh;
-	fh = (char*)malloc(sizeof(char) * 256);
-	fh = fgets(buffer, 256, input_file);
-	// If the file is empty
-	if(fh == NULL){
-		// ERROR: File is empty.
-		printf("ERROR");
-	}
-	// If the ppm file does not start with P6
-	// If the magic number is not correct.
-	if(buffer[0] != 'P' || buffer[1] != '6'){
-		// Still output the correct magic number.
-		// Let the user know that their file might be bad.
-		fprintf(output_file, "P3\n");
+	int line_num = 0;
+	char line[256], tmp_width[256]="", tmp_height[256]="", tmp_max[256]="";
+	char magic_num[3];
+	int data_count = 0;
+	header ppm_header;
+	int field = 1;
+	// If the ppm file does not start with P6 or P3
+	fgets(line, 256, input_file);
+	if(line[0] != 'P'){
+		if(line[1] != '3'){
+			printf("ERROR");
+		}		
 		printf("ERROR");
 	}
 	else{
-		// remove the magic number from buffer and output it to the new file.
-		*(buffer+=2);
-		fprintf(output_file, "P3\n");
-		//printf("%c", buffer[0]);
-		if(){
-			*(buffer += 1);
-			printf("didnt crash");
-			//buffer+=1;
+		// grab the header
+		if(field == 1){
+			ppm_header.magic_num = malloc(3);
+			sprintf(magic_num, "%c%c", line[0], line[1]);
+			strcpy(ppm_header.magic_num, magic_num);
+			//printf(ppm_header.magic_num);
+			fprintf(output_file, "P6\n");
+			free(ppm_header.magic_num);
+			field++;
 		}
-		//while(n == ' ' || n == '\n'){
-		//	//buffer += 1;
-		//	printf("wtf");
-		//}
-		//while(getc(buffer) == '\n'){
-		//	buffer++;
-		//}
-		printf("%c", buffer[0]);
-	}
+		int i = 2;
+		int header_lines=0;
+		while(i < strlen(line)){
+			if(isdigit(line[i])){
+				while(isdigit(line[i])){
+					if(field == 2){
+						tmp_width[strlen(tmp_width)] = line[i];
+						// fill width;
+						//printf("%c", line[i]);
+					}
+					if(field == 3){
+						tmp_height[strlen(tmp_height)] = line[i];
+						// fill height
+						//printf("%c", line[i]);
+					}
+					if(field == 4){
+						tmp_max[strlen(tmp_max)] = line[i];
+						// fill max size
+						//printf("%c", line[i]);
+					}
+					i++;
+				}
+				field++;
+				//printf("\n");
+			}
+			// eat a character if it is a space
+			if(line[i] == ' '){
+				i++;
+				continue;
+			}
+			// if its a newline or comment go to the next line.
+			if(line[i] == '\n' || line[i] == '#'){
+				fgets(line, 256, input_file);
+				header_lines++;
+				//printf("%s", line);
+				i=0;
+				continue;
+			}
+			i++;
+		}
+		// Finish building string
+		tmp_width[strlen(tmp_width)+1] = '\0';
+		tmp_height[strlen(tmp_height)+1] = '\0';
+		tmp_max[strlen(tmp_max)+1] = '\0';
+		ppm_header.width = atoi(tmp_width);
+		ppm_header.height = atoi(tmp_height);
+		if(atoi(tmp_max) != 255){
+			printf("ERROR Not 8 bit color");
+			return -1;
+		}
+		else{
+			ppm_header.data = (Pixel *)malloc(sizeof(Pixel) * ppm_header.width * ppm_header.height);
+			ppm_header.max_num = atoi(tmp_max);
+			fprintf(output_file, "%u %u\n%u\n", ppm_header.width, ppm_header.height, ppm_header.max_num);
+			//read the image into the buffer
+			fread(ppm_header.data, sizeof(Pixel), ppm_header.width * ppm_header.height, input_file);
+			for (i = 0; i < ppm_header.width * ppm_header.height; i++){
+				int c_byte;
+				fscanf(input_file, "%d", &c_byte);
+				ppm_header.data[i].r = c_byte;
+				fscanf(input_file, "%d", &c_byte);
+				ppm_header.data[i].g = c_byte;
+				fscanf(input_file, "%d", &c_byte);
+				ppm_header.data[i].b = c_byte;
+			}
+			//write the binary out
+			fwrite(ppm_header.data, 2 * ppm_header.width, ppm_header.height, output_file);
+			return 0;
+		}
+		printf("%d\n", ppm_header.width);
+		printf("%d\n", ppm_header.height);
+		printf("%d\n", ppm_header.max_num);
+	}	//parse_data(input_file);
 }
+int p6_p3(FILE* input_file, FILE* output_file){
+	int line_num = 0;
+	char line[256], tmp_width[256]="", tmp_height[256]="", tmp_max[256]="";
+	char magic_num[3];
+	int data_count = 0;
+	header ppm_header;
+	int field = 1;
+	// If the ppm file does not start with P6 or P3
+	fgets(line, 256, input_file);
+	if(line[0] != 'P'){
+		if(line[1] != '6' || line[1] != '3'){
+			printf("ERROR");
+		}		
+		printf("ERROR");
+	}
+	else{
+		// grab the header
+		if(field == 1){
+			ppm_header.magic_num = malloc(3);
+			sprintf(magic_num, "%c%c", line[0], line[1]);
+			strcpy(ppm_header.magic_num, magic_num);
+			//printf(ppm_header.magic_num);
+			fprintf(output_file, "P3\n");
+			free(ppm_header.magic_num);
+			field++;
+		}
+		int i = 2;
+		int header_lines=0;
+		while(i < strlen(line)){
+			if(isdigit(line[i])){
+				while(isdigit(line[i])){
+					if(field == 2){
+						tmp_width[strlen(tmp_width)] = line[i];
+						// fill width;
+						//printf("%c", line[i]);
+					}
+					if(field == 3){
+						tmp_height[strlen(tmp_height)] = line[i];
+						// fill height
+						//printf("%c", line[i]);
+					}
+					if(field == 4){
+						tmp_max[strlen(tmp_max)] = line[i];
+						// fill max size
+						//printf("%c", line[i]);
+					}
+					i++;
+				}
+				field++;
+				//printf("\n");
+			}
+			// eat a character if it is a space
+			if(line[i] == ' '){
+				i++;
+				continue;
+			}
+			// if its a newline or comment go to the next line.
+			if(line[i] == '\n' || line[i] == '#'){
+				fgets(line, 256, input_file);
+				header_lines++;
+				//printf("%s", line);
+				i=0;
+				continue;
+			}
+			i++;
+		}
+		// Finish building string
+		tmp_width[strlen(tmp_width)+1] = '\0';
+		tmp_height[strlen(tmp_height)+1] = '\0';
+		tmp_max[strlen(tmp_max)+1] = '\0';
+		ppm_header.width = atoi(tmp_width);
+		ppm_header.height = atoi(tmp_height);
+		if(atoi(tmp_max) != 255){
+			printf("ERROR Not 8 bit color");
+			return -1;
+		}
+		else{
+			ppm_header.data = (Pixel *)malloc(sizeof(Pixel) * ppm_header.width * ppm_header.height);
+			ppm_header.max_num = atoi(tmp_max);
+			fprintf(output_file, "%u %u\n%u\n", ppm_header.width, ppm_header.height, ppm_header.max_num);
+			//read the image into the buffer
+			fread(ppm_header.data, sizeof(Pixel), ppm_header.width * ppm_header.height, input_file);
+			int counter = 1;
+			//write out to the ascii file
+			for (i = 0; i < ppm_header.width * ppm_header.height; i++){
+				fprintf(output_file, "%d %d %d ", ppm_header.data[i].r, ppm_header.data[i].g, ppm_header.data[i].b);
+
+				// If the counter is at the end of the width of the ppm
+				if(counter == ppm_header.width) {
+					fprintf(output_file, "\n");
+					counter = 1;
+				}
+				else counter += 1;
+			}
+			return 0;
+		}
+		printf("%d\n", ppm_header.width);
+		printf("%d\n", ppm_header.height);
+		printf("%d\n", ppm_header.max_num);
+	}	//parse_data(input_file);
+}
+
+
 int main(int argc, char *argv[]){
 	FILE *output_file = fopen(argv[3], "wb");
 	FILE *input_file = fopen(argv[2], "rb");
-  
-	char fh[256];
-	// Check for correct number of arguments
+  	// Check for correct number of arguments
 	if(argc != 4){
 		// ERROR: This program takes 3 and only 3 arguments.
 		// Close Files
